bc79dd41ecd02ba45727f0382acdad7960ed27ef
Wiive <wirrechan@hotmail.com>
2017-10-26 13:47:30 +0200

    Marge

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/TAG/DropOffZone.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TAG_DropOffZone_generated_h
+#error "DropOffZone.generated.h already included, missing '#pragma once' in DropOffZone.h"
+#endif
+#define TAG_DropOffZone_generated_h
+
+#define TAG_Source_TAG_DropOffZone_h_12_RPC_WRAPPERS
+#define TAG_Source_TAG_DropOffZone_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define TAG_Source_TAG_DropOffZone_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesADropOffZone(); \
+	friend TAG_API class UClass* Z_Construct_UClass_ADropOffZone(); \
+public: \
+	DECLARE_CLASS(ADropOffZone, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/TAG"), NO_API) \
+	DECLARE_SERIALIZER(ADropOffZone) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TAG_Source_TAG_DropOffZone_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesADropOffZone(); \
+	friend TAG_API class UClass* Z_Construct_UClass_ADropOffZone(); \
+public: \
+	DECLARE_CLASS(ADropOffZone, AActor, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/TAG"), NO_API) \
+	DECLARE_SERIALIZER(ADropOffZone) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define TAG_Source_TAG_DropOffZone_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ADropOffZone(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ADropOffZone) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADropOffZone); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADropOffZone); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ADropOffZone(ADropOffZone&&); \
+	NO_API ADropOffZone(const ADropOffZone&); \
+public:
+
+
+#define TAG_Source_TAG_DropOffZone_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ADropOffZone(ADropOffZone&&); \
+	NO_API ADropOffZone(const ADropOffZone&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ADropOffZone); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ADropOffZone); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ADropOffZone)
+
+
+#define TAG_Source_TAG_DropOffZone_h_12_PRIVATE_PROPERTY_OFFSET
+#define TAG_Source_TAG_DropOffZone_h_9_PROLOG
+#define TAG_Source_TAG_DropOffZone_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TAG_Source_TAG_DropOffZone_h_12_PRIVATE_PROPERTY_OFFSET \
+	TAG_Source_TAG_DropOffZone_h_12_RPC_WRAPPERS \
+	TAG_Source_TAG_DropOffZone_h_12_INCLASS \
+	TAG_Source_TAG_DropOffZone_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TAG_Source_TAG_DropOffZone_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TAG_Source_TAG_DropOffZone_h_12_PRIVATE_PROPERTY_OFFSET \
+	TAG_Source_TAG_DropOffZone_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	TAG_Source_TAG_DropOffZone_h_12_INCLASS_NO_PURE_DECLS \
+	TAG_Source_TAG_DropOffZone_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TAG_Source_TAG_DropOffZone_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
