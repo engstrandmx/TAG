7e79f717654712864e9001b97609a4248db6a422
ritli <agnar_oskarsson@hotmail.com>
2017-11-30 18:53:31 +0100

    Basic destructible component functionality added

--- Source/TAG/DestructibleSceneComponent.cpp
+++ Source/TAG/DestructibleSceneComponent.cpp
@@ -9,18 +9,32 @@ UDestructibleSceneComponent::UDestructibleSceneComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	bDestroyMesh = true;
 
+	Health = 10;
 	// ...
 }
 
+void UDestructibleSceneComponent::DestroyObject(float Damage)
+{
+	CurrentHealth -= Damage;
+
+	if (ParticleEmitter) {
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ParticleEmitter, GetComponentTransform(), true);
+	}
+	
+	if (bDestroyMesh && CurrentHealth <= 0) {
+		GetAttachmentRootActor()->Destroy();
+	}
+}
 
 // Called when the game starts
 void UDestructibleSceneComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	CurrentHealth = Health;
 	// ...
-	
 }
 
 
--- Source/TAG/DestructibleSceneComponent.h
+++ Source/TAG/DestructibleSceneComponent.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Engine.h"
 #include "Components/SceneComponent.h"
 #include "DestructibleSceneComponent.generated.h"
 
@@ -13,17 +13,31 @@ class TAG_API UDestructibleSceneComponent : public USceneComponent
 	GENERATED_BODY()
 
 public:	
+
+	UPROPERTY(EditAnywhere)
+	float Health;
+
+	UPROPERTY(EditAnywhere)
+	bool bDestroyMesh;
+
+	UPROPERTY(EditAnywhere)
+	UParticleSystem* ParticleEmitter;
 	// Sets default values for this component's properties
 	UDestructibleSceneComponent();
+
+	UFUNCTION(BlueprintCallable)
+	void DestroyObject(float Damage);
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+private:
+	float CurrentHealth;
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 		
 	
 };
