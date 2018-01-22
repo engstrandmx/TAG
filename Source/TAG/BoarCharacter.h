d1d0b821829da0b15c25fac3b1e25a81471cc184
ritli <agnar_oskarsson@hotmail.com>
2018-01-22 18:14:09 +0100

    Fixed spawned piglets not moving, added piglet explosions

--- Source/TAG/BoarCharacter.h
+++ Source/TAG/BoarCharacter.h
@@ -3,6 +3,9 @@
 #pragma once
 
 #include "Engine.h"
+#include "PigletCharacter.h"
+#include "PigletAIController.h"
+
 #include "GameFramework/Character.h"
 #include "BoarCharacter.generated.h"
 
@@ -12,16 +15,36 @@ class TAG_API ABoarCharacter : public ACharacter
 	GENERATED_BODY()
 
 	ABoarCharacter();
+
 	
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USphereComponent* SphereCollider;
 
 public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Spawnables)
+	TSubclassOf<APigletCharacter> PigletMob;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Spawnables)
+	TSubclassOf<APigletAIController> PigletController;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Spawnables)
+	TArray<APigletCharacter*> Piglets;
+
+	UFUNCTION(BlueprintCallable, Category = Spawnables)
+	APigletCharacter* SpawnPiglet(FVector Offset, FRotator Rotation);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
 	bool bStunned;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
 	bool bCharging;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
+	int Health = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
+	int CurrentPhase = 1;
+
 
 };
--- Source/TAG/PigletCharacter.h
+++ Source/TAG/PigletCharacter.h
@@ -27,5 +27,5 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	
-	
+
 };
