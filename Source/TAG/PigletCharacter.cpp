31e4331f43610740e9200d3754ff7f60b7c62327
ritli <agnar_oskarsson@hotmail.com>
2018-01-22 15:01:34 +0100

    Implemented basic Piglet AI

---
+++ Source/TAG/PigletCharacter.cpp
@@ -0,0 +1,29 @@
+
+
+#include "PigletCharacter.h"
+
+
+APigletCharacter::APigletCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void APigletCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APigletCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void APigletCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
