79a1d08509988134047388a8cfe6e0670d8ce4cd
RebeccaRomin <rebecca@galejan.eu>
2018-04-06 10:58:05 +0200

    Update from master

--- Source/TAG/TAGGameMode.h
+++ Source/TAG/TAGGameMode.h
@@ -22,11 +22,14 @@ class ATAGGameMode : public AGameModeBase
 public:
 	ATAGGameMode();
 
+	//The type of character being controlled at the moment. Used to determine which character to switch to.
 	PlayerType CurrentPlayerType = PlayerType::Troll;
 
+	//References to the current spawned characters of each type.
 	AGnomeCharacter* CurrentGnome;
 	ATrollCharacter* CurrentTroll;
 
+	//Which pawn to spawn when respawning and starting the game.
 	UPROPERTY(EditAnywhere, Category = Characters)
 	TSubclassOf<APawn> TrollCharacter;
 	UPROPERTY(EditAnywhere, Category = Characters)
@@ -34,39 +37,45 @@ public:
 	UPROPERTY(EditAnywhere, Category = Characters)
 	TSubclassOf<APawn> SpectatorCharacter;
 
+	//Legacy, used when gnome and troll had different spawns
 	UPROPERTY(EditAnywhere, Category = Characters)
 	FString TrollSpawnTag = "SpawnTroll";
 	UPROPERTY(EditAnywhere, Category = Characters)
 	FString GnomeSpawnTag = "SpawnGnome";
 
+	//Initial spawnpoint, this is where player will spawn when first starting the game
 	UPROPERTY(EditAnywhere, Category = Spawn)
 	AActor* StartingSpawnPoint;
 
+	//Amount of collectibles collected, this is reset between levels at the moment
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Collectibles)
 	int CollectibleCount;
+	//The amount of collectibles in the loaded level, this is set at BeginPlay()
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Collectibles)
 	int CollectiblesInLevel;
 
-	void RestartPlayer(AController* NewPlayer);
-
-	FORCEINLINE void SetCurrentGnome(AGnomeCharacter* Gnome) { CurrentGnome = Gnome; }
-	FORCEINLINE void SetCurrentTroll(ATrollCharacter* Troll) { CurrentTroll = Troll; }
-	FORCEINLINE void SetCurrentPlayerType(PlayerType Type) { CurrentPlayerType = Type; }
-	FORCEINLINE void SetCurrentCheckpoints(TArray<APlayerStart*> InArray) { CurrentPlayerStarts = InArray; }
-	
-	UFUNCTION(BlueprintCallable, Category = Collectibles)
-	void PickupCollectible();
-
+	//Getters
+	UFUNCTION(BlueprintCallable, Category = Player)
+	FORCEINLINE	TEnumAsByte<EPlayerType::PlayerType> GetCurrentPlayerType() { return CurrentPlayerType; }
 
 	UFUNCTION(BlueprintCallable, Category = "Gameplay Actors")
 	FORCEINLINE ATrollCharacter* GetTrollCharacter() { return CurrentTroll; }
 	UFUNCTION(BlueprintCallable, Category = "Gameplay Actors")
 	FORCEINLINE AGnomeCharacter* GetGnomeCharacter() { return CurrentGnome; }
 
-	UFUNCTION(BlueprintCallable, Category = Player)
-	FORCEINLINE	TEnumAsByte<EPlayerType::PlayerType> GetCurrentPlayerType(){ return CurrentPlayerType; }
-	
+	FORCEINLINE void SetCurrentGnome(AGnomeCharacter* Gnome) { CurrentGnome = Gnome; }
+	FORCEINLINE void SetCurrentTroll(ATrollCharacter* Troll) { CurrentTroll = Troll; }
+	FORCEINLINE void SetCurrentPlayerType(PlayerType Type) { CurrentPlayerType = Type; }
+	FORCEINLINE void SetCurrentCheckpoints(TArray<APlayerStart*> InArray) { CurrentPlayerStarts = InArray; }
 
+	//This is called every time a character is respawned or during initial spawn
+	void RestartPlayer(AController* NewPlayer);
+
+	//Called by collectible blueprint when player picks up a collectible
+	UFUNCTION(BlueprintCallable, Category = Collectibles)
+	void PickupCollectible();
+
+	//Blueprint events, some legacy
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Events")
 		void OnPreGameStart();
 
@@ -93,6 +102,7 @@ public:
 
 private:
 
+	//Array of checkpoints the player can spawn at. These are set by the checkpointfield blueprint 
 	TArray<APlayerStart*> CurrentPlayerStarts;
 
 	AActor* FindPlayerStart_Implementation(AController* Player, const FString& IncomingName) override;
